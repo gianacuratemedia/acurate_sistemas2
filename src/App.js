73808bd6bf3427fe1bc056472919805862025c48
Alejandro <alejandro_gh19@hotmail.com>
2020-10-12 19:03:17 -0500

    subiendo Segundos archivos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import './assets/css/App.css';
-import './assets/css/stylesgalery.css';
+import './assets/css/stylesprofesores.css';
 import imagenes from './assets/img/imagenes';
 import{FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faSearch}from '@fortawesome/free-solid-svg-icons';
@@ -12,7 +12,7 @@ function App(){
   return (
     
     <div className="header">
-      <img className="fondo" src={imagenes.img}/>
+      <img className="fondo" src={imagenes.img4}/>
    
 
     <div className="boton-unete">
@@ -50,14 +50,51 @@ function App(){
              placeholder="Encuentra cursos, certificaciones y profesores.."
               className="buscador"/>
         
-          
-          
         
-
-    
     </div>
 
+
+<div className="texto2">
+<h1>Conoce a tus profesores</h1>
+
+</div>
+
+    <div className="container">
+  
+        <div className="item">
+          <img className="profe1" src={imagenes.img5}/>
+            <div className="item-text">
+              <h3><a href="dfgdfg">Nombre de Instructor</a></h3>
+                <div className="sub-text">
+                 <h5>Loremp ipsun</h5>
+                </div>
+            </div>
+        </div>
+        <div className="item">
+          <img className="profe1" src={imagenes.img5}/>
+            <div className="item-text">
+              <h3><a href="dfgdfgd">Nombre de Instructor</a></h3>
+              <div className="sub-text">
+                 <h5>Loremp ipsun</h5>
+                </div>
+            </div>
+        </div>
+        <div className="item">
+          <img className="profe1" src={imagenes.img5}/>
+            <div className="item-text">
+              <h3><a href="fdgdf">Nombre de Instructor</a></h3>
+              <div className="sub-text">
+                 <h5>Loremp ipsun</h5>
+                </div>
+            </div>
+        </div>
     </div>
+
+
+    </div>
+
+
+
   );
 }
 
--- src/assets/img/imagenes.js
+++ src/assets/img/imagenes.js
@@ -1,12 +1,15 @@
 import azul from'./knowtureazul.png';
 import blanco from './knowtureblanco.png';
 import fondo from './fondo1.jpg';
-import fondo2 from './fondo.jpg';
+import fondo2 from './fondo2.jpg';
+import profesor from './persona1.jpg';
+
 
 export default {
 
     "img1": azul,
     "img2": blanco,
     "img3": fondo,
-    "img4": fondo2
+    "img4": fondo2,
+    "img5": profesor
 }
