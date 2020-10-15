1fab24f4e6741554a13f5f8e0ba1f66f5c4738a0
JavierPinedoC <a01551344@itesm.mx>
2020-10-15 01:53:34 -0500

    Se ajustaron los componentes de la página principal

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./assets/css/App.css";
 import "./assets/css/stylesprofesores.css";
 import imagenes from "./assets/img/imagenes";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   document.addEventListener("DOMContentLoaded", function () {
@@ -65,7 +65,7 @@ function App() {
     //       <img className="profe1" src={imagenes.img5} />
     //       <div className="item-text">
     //         <h3>
-    //           <a href="dfgdfg">Nombre de Instructor</a>
+    //           <a href="#">Nombre de Instructor</a>
     //         </h3>
     //         <div className="sub-text">
     //           <h5>Loremp ipsun</h5>
@@ -76,7 +76,7 @@ function App() {
     //       <img className="profe1" src={imagenes.img5} />
     //       <div className="item-text">
     //         <h3>
-    //           <a href="dfgdfgd">Nombre de Instructor</a>
+    //           <a href="#d">Nombre de Instructor</a>
     //         </h3>
     //         <div className="sub-text">
     //           <h5>Loremp ipsun</h5>
@@ -102,38 +102,108 @@ function App() {
         <div class="parallax">
           <img src={imagenes.img4} />
         </div>
-        <nav className="transparent">
+        <nav className="transparent z-depth-0">
           <div class="nav-wrapper">
             <a href="#" class="brand-logo">
-            <img className="azul responsive-img"  src={imagenes.img2} />
+              <img className="azul responsive-img" src={imagenes.img2} />
             </a>
             <ul id="nav-mobile" class="right hide-on-med-and-down">
               <li>
-                <a href="sass.html">Sass</a>
+                <a href="#">Iniciar Sesión</a>
               </li>
               <li>
-                <a href="badges.html">Components</a>
-              </li>
-              <li>
-                <a href="collapsible.html">JavaScript</a>
+                <a href="#" className="boton-unete">
+                  ¡Unete!
+                </a>
               </li>
             </ul>
           </div>
         </nav>
+
+        <div className="texto">
+          <h2>
+            Aprende de manera eficiente,<br></br>
+            con los mejores profesores.
+          </h2>
+        </div>
+        <p className="parrafoPrincipal">
+          Somos una plataforma de contenido en<br></br>
+          streaming. Nuestros programas permiten terminar<br></br>
+          cursos enteros en 15 minutos en el formato que<br></br>
+          prefieran con mas de 600 contenidos en áreas
+        </p>
+
+        <div>
+          <a className="boton" href="#">
+            ¡Unete a nosotros!
+          </a>
+        </div>
+
+        <form>
+          <div className="barraBusqueda">
+            <label class="label-icon" for="search">
+              <i class="material-icons">search</i>
+            </label>
+            <input
+              id="search"
+              type="search"
+              placeholder="Encuentra cursos, certificaciones y profesores.."
+              required
+            />
+          </div>
+        </form>
       </div>
+
       <div class="section white">
         <div class="row container">
-          <h2 class="header">Parallax</h2>
-          <p class="grey-text text-darken-3 lighten-3">
-            Parallax is an effect where the background content or image in this
-            case, is moved at a different speed than the foreground content
-            while scrolling.
-          </p>
+          <h2 class="header center">Conoce a tus profesores</h2>
+
+          <div className="col s4">
+            <div className="item center">
+              <img className="profe1" src={imagenes.img5} />
+              <div className="item-text">
+                <h3>
+                  <a href="#">Nombre de Instructor</a>
+                </h3>
+                <div className="sub-text">
+                  <h5>Loremp ipsun</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col s4">
+            <div className="item center">
+              <img className="profe1" src={imagenes.img5} />
+              <div className="item-text">
+                <h3>
+                  <a href="#">Nombre de Instructor</a>
+                </h3>
+                <div className="sub-text">
+                  <h5>Loremp ipsun</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col s4">
+            <div className="item center">
+              <img className="profe1" src={imagenes.img5} />
+              <div className="item-text">
+                <h3>
+                  <a href="#">Nombre de Instructor</a>
+                </h3>
+                <div className="sub-text">
+                  <h5>Loremp ipsun</h5>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+
       <div class="parallax-container">
         <div class="parallax">
-          {" "}
           <img src={imagenes.img4} />
         </div>
       </div>
