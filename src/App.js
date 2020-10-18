14843d055dd89bde63887eb3d0b5c7634dfb75b0
JavierPinedoC <a01551344@itesm.mx>
2020-10-18 17:41:24 -0500

    Se agregaron las rutas

--- src/App.js
+++ src/App.js
@@ -1,214 +1,25 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./assets/css/App.css";
-import "./assets/css/stylesprofesores.css";
-import imagenes from "./assets/img/imagenes";
-import "materialize-css/dist/css/materialize.min.css";
-import M from "materialize-css";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+
+// components
+import index from "./pages/IndexLayout/indexLayout";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 function App() {
-  document.addEventListener("DOMContentLoaded", function () {
-    var elems = document.querySelectorAll(".parallax");
-    var instances = M.Parallax.init(elems);
-  });
   return (
-    // <div className="header">
-    //   <img className="fondo" src={imagenes.img4} />
-
-    //   <div className="boton-unete">
-    //     <a className="boton-iniciar" href="">
-    //       Iniciar Sesion
-    //     </a>
-    //     <a className="boton-unete" href="">
-    //       ¡Unete!
-    //     </a>
-    //   </div>
-
-    //   <img className="azul" src={imagenes.img2} />
-
-    //   <div className="texto">
-    //     <h1>
-    //       Aprende de manera eficiente,<br></br>
-    //       con los mejores profesores.
-    //     </h1>
-    //   </div>
-
-    //   <p className="parrafoPrincipal">
-    //     Somos una plataforma de contenido en<br></br>
-    //     streaming. Nuestros programas permiten terminar<br></br>
-    //     cursos enteros en 15 minutos en el formato que<br></br>
-    //     prefieran con mas de 600 contenidos en áreas
-    //   </p>
-
-    //   <div>
-    //     <a className="boton" href="">
-    //       ¡Unete a nosotros!
-    //     </a>
-    //   </div>
-
-    //   <div className="barraBusqueda">
-    //     <FontAwesomeIcon icon={faSearch} className="icon" />
-    //     <input
-    //       type="text"
-    //       placeholder="Encuentra cursos, certificaciones y profesores.."
-    //       className="buscador"
-    //     />
-    //   </div>
-
-    //   <div className="texto2">
-    //     <h1>Conoce a tus profesores</h1>
-    //   </div>
-
-    //   <div className="container">
-    //     <div className="item">
-    //       <img className="profe1" src={imagenes.img5} />
-    //       <div className="item-text">
-    //         <h3>
-    //           <a href="#">Nombre de Instructor</a>
-    //         </h3>
-    //         <div className="sub-text">
-    //           <h5>Loremp ipsun</h5>
-    //         </div>
-    //       </div>
-    //     </div>
-    //     <div className="item">
-    //       <img className="profe1" src={imagenes.img5} />
-    //       <div className="item-text">
-    //         <h3>
-    //           <a href="#d">Nombre de Instructor</a>
-    //         </h3>
-    //         <div className="sub-text">
-    //           <h5>Loremp ipsun</h5>
-    //         </div>
-    //       </div>
-    //     </div>
-    //     <div className="item">
-    //       <img className="profe1" src={imagenes.img5} />
-    //       <div className="item-text">
-    //         <h3>
-    //           <a href="fdgdf">Nombre de Instructor</a>
-    //         </h3>
-    //         <div className="sub-text">
-    //           <h5>Loremp ipsun</h5>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
-
-    <div>
-      <div class="parallax-container">
-        <div class="parallax">
-          <img src={imagenes.img4} />
-        </div>
-        <nav className="transparent z-depth-0">
-          <div class="nav-wrapper">
-            <a href="#" class="brand-logo">
-              <img className="azul responsive-img" src={imagenes.img2} />
-            </a>
-            <ul id="nav-mobile" class="right hide-on-med-and-down">
-              <li>
-                <a href="#">Iniciar Sesión</a>
-              </li>
-              <li>
-                <a href="#" className="boton-unete">
-                  ¡Unete!
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-
-        <div className="texto">
-          <h2>
-            Aprende de manera eficiente,<br></br>
-            con los mejores profesores.
-          </h2>
-        </div>
-        <p className="parrafoPrincipal">
-          Somos una plataforma de contenido en<br></br>
-          streaming. Nuestros programas permiten terminar<br></br>
-          cursos enteros en 15 minutos en el formato que<br></br>
-          prefieran con mas de 600 contenidos en áreas
-        </p>
-
-        <div>
-          <a className="boton" href="#">
-            ¡Unete a nosotros!
-          </a>
-        </div>
-
-        <form>
-          <div className="barraBusqueda">
-            <label class="label-icon" for="search">
-              <i class="material-icons">search</i>
-            </label>
-            <input
-              id="search"
-              type="search"
-              placeholder="Encuentra cursos, certificaciones y profesores.."
-              required
-            />
-          </div>
-        </form>
-      </div>
-
-      <div class="section white">
-        <div class="row container">
-          <h2 class="header center">Conoce a tus profesores</h2>
-
-          <div className="col s4">
-            <div className="item center">
-              <img className="profe1" src={imagenes.img5} />
-              <div className="item-text">
-                <h3>
-                  <a href="#">Nombre de Instructor</a>
-                </h3>
-                <div className="sub-text">
-                  <h5>Loremp ipsun</h5>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="col s4">
-            <div className="item center">
-              <img className="profe1" src={imagenes.img5} />
-              <div className="item-text">
-                <h3>
-                  <a href="#">Nombre de Instructor</a>
-                </h3>
-                <div className="sub-text">
-                  <h5>Loremp ipsun</h5>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="col s4">
-            <div className="item center">
-              <img className="profe1" src={imagenes.img5} />
-              <div className="item-text">
-                <h3>
-                  <a href="#">Nombre de Instructor</a>
-                </h3>
-                <div className="sub-text">
-                  <h5>Loremp ipsun</h5>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="parallax-container">
-        <div class="parallax">
-          <img src={imagenes.img4} />
-        </div>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={index} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
