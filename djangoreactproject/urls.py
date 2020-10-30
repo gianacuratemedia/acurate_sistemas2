6c37b7c485ee161b15701690618936a66dd9284e
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-10-30 14:30:01 -0600

    Update urls.py

--- djangoreactproject/urls.py
+++ djangoreactproject/urls.py
@@ -22,7 +22,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-       path('accounts/', include('accounts.urls')),
+      
     
     
   
