3b95d93d9516e7856df22a9f06a914112927cccb
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-10-30 14:22:46 -0600

    Update models.py

--- Usuarios_k/models.py
+++ Usuarios_k/models.py
@@ -1,9 +1,41 @@
-from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.contrib.auth.models import AbstractUser, PermissionsMixin, AbstractBaseUser, BaseUserManager
+
 from django.db import models
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.utils.translation import gettext_lazy as _
 
-class CustomUser(AbstractUser, PermissionsMixin):
+
+
+
+#MODELO SUPERUSUARIO
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, email, password=None):
+        if username is None:
+            raise TypeError('Los usuarios deben tener un nombre de usuario')
+        if email is None:
+            raise TypeError('Los usuarios deben tener un email')
+
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        if password is None:
+            raise TypeError('Debes ingresar una contraseña')
+
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        return user
+
+
+#MODELO CUSTOMUSER
+
+
+class CustomUser(AbstractUser):
     nombre=models.CharField(null= True, max_length=150)
     apellido_paterno=models.CharField(null= True, max_length=150)
     apellido_materno=models.CharField(null= True, max_length=150)
@@ -13,7 +45,7 @@ class CustomUser(AbstractUser, PermissionsMixin):
     email = models.EmailField(_('direccion de email'), unique=True)
     telefono=models.CharField(null=False, max_length=12)
     authkey=models.CharField(null= True, max_length=150)
-    token=models.CharField(null= True, max_length=150)
+    token=models.CharField(null= True, max_length=555)
     is_verified = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     password=models.CharField(null=False, max_length=40)
@@ -29,31 +61,34 @@ class CustomUser(AbstractUser, PermissionsMixin):
     fecha_registro=models.DateTimeField(auto_now_add=True)
     ultima_visita=models.DateTimeField(auto_now=True)
     dias_premium=models.IntegerField(null=True)
-
+   
+   
     def register(self):
         self.save()
 
-    @staticmethod
-    def get_customer_by_email(email):
+@staticmethod
+def get_customer_by_email(email):
         try:
             return CustomUser.objects.get(email=email)
         except:
             return False
 
 
-    def isExists(self):
+def isExists(self):
         if CustomUser.objects.filter(email = self.email):
             return True
 
         return  False
 
 
-    def __str__(self):
+def __str__(self):
         return self.email
 
-    def tokens(self):
+def tokens(self):
         refresh = RefreshToken.for_user(self)
         return {
             'refresh': str(refresh),
             'access': str(refresh.access_token)
         }
+
+        
