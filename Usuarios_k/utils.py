8f1038e15e9398bddf816af627f85b01a262c98e
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-11-03 16:36:35 -0600

    Update utils.py

--- Usuarios_k/utils.py
+++ Usuarios_k/utils.py
@@ -1,11 +1,22 @@
-from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from six import text_type
+from django.core.mail import EmailMessage
 
 
-class AppTokenGenerator(PasswordResetTokenGenerator):
-
-    def _make_hash_value(self, user, timestamp):
-        return (text_type(user.is_active) + text_type(user.pk) + text_type(timestamp))
+import threading
 
 
-account_activation_token = AppTokenGenerator()
+class EmailThread(threading.Thread):
+
+    def __init__(self, email):
+        self.email = email
+        threading.Thread.__init__(self)
+
+    def run(self):
+        self.email.send()
+
+
+class Util:
+    @staticmethod
+    def send_email(data):
+        email = EmailMessage(
+            subject=data['email_subject'], body=data['email_body'], to=[data['to_email']])
+        EmailThread(email).start()
