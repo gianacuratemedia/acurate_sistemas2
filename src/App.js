749b393f2dd85b19b606e8f6867c9f6cd2a299fc
JavierPinedoC <a01551344@itesm.mx>
2020-11-06 12:54:15 -0600

    agregar página pay

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import "./assets/css/App.css";
 import index from "./pages/IndexLayout/indexLayout";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
-import Pay from './pages/pay/pay';
 import PlanSelection from "./pages/Plan Selection/PlanSelection";
 
 function App() {
@@ -17,7 +16,6 @@ function App() {
         <Route exact path="/" component={index} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
-        <Route exact path="/pay" component={Pay}/>
         <Route exact path="/planselection" component={PlanSelection} />
       </Switch>
     </BrowserRouter>
