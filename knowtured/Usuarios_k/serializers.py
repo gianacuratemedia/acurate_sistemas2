6c82dcd2e69c5c07ae395a01c78f54801466f746
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-11-12 15:31:26 -0600

    Update serializers.py

--- knowtured/Usuarios_k/serializers.py
+++ knowtured/Usuarios_k/serializers.py
@@ -28,11 +28,92 @@ class CustomUserSerializer(serializers.ModelSerializer):
         return customUser
 
     #Verificacion de cuenta:
-    class EmailVerificationSerializer(serializers.ModelSerializer):
+class EmailVerificationSerializer(serializers.ModelSerializer):
         token = serializers.CharField(max_length=555)
         class Meta:
           model = CustomUser
         fields = ['token']
+
+#Login
+class LoginSerializer(serializers.ModelSerializer):
+    nombre_usuario = serializers.CharField(
+        max_length=255, min_length=3, read_only=True)
+    email = serializers.EmailField(max_length=255, min_length=3)
+    password = serializers.CharField(
+        max_length=68, min_length=6, write_only=True)
+    
+
+    tokens = serializers.SerializerMethodField()
+
+    def get_tokens(self, obj):
+        customUser = CustomUser.objects.get(email=obj['email'])
+
+        return {
+            'refresh': customUser.tokens()['refresh'],
+            'access': customUser.tokens()['access']
+        }
+
+    class Meta:
+        model = CustomUser
+        fields = ['nombre_usuario','email', 'password', 'tokens']
+
+    def validate(self, attrs):
+         email = attrs.get('email', '')
+         password = attrs.get('password', '')
+         filtered_user_by_email = CustomUser.objects.filter(email=email)
+         customUser = auth.authenticate(email=email, password=password)
+
+         if filtered_user_by_email.exists() and filtered_user_by_email[0].auth_provider != 'email':
+            raise AuthenticationFailed(
+                detail='Continúe con su inicio de sesión usando ' + filtered_user_by_email[0].auth_provider)
+
+         if not customUser:
+            raise AuthenticationFailed('Credenciales inválidas, intentelo de nuevo')
+         if not customUser.is_active:
+            raise AuthenticationFailed('Cuenta inhabilitada, contacta con el administrador')
+         if not customUser.is_verified:
+            raise AuthenticationFailed('Email no verificado')
+
+         return {
+            'email': customUser.email,
+            'nombre_usuario': customUser.nombre_usuario,
+            'tokens': customUser.tokens
+        }
+      
+        
+         return super().validate(attrs)
+         
+#Cerrar sesión
+
+class LogoutSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
+
+    default_error_message = {
+        'bad_token': ('El token ha expirado o es inválido')
+    }
+
+    def validate(self, attrs):
+        self.token = attrs['refresh']
+        return attrs
+
+    def save(self, **kwargs):
+
+        try:
+            RefreshToken(self.token).blacklist()
+
+        except TokenError:
+            self.fail('bad_token')
+
+
+# Cambiar password
+class ResetPasswordEmailRequestSerializer(serializers.Serializer):
+    email = serializers.EmailField(min_length=2)
+
+    redirect_url = serializers.CharField(max_length=500, required=False)
+
+    class Meta:
+        fields = ['email']
+
 
 #Login
 class LoginSerializer(serializers.ModelSerializer):
