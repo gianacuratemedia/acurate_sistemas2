4dc2b224da83023182f1004ac1e652ebc49aa006
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-11-12 15:12:10 -0600

    Update views.py

--- knowtured/Usuarios_k/views.py
+++ knowtured/Usuarios_k/views.py
@@ -1,55 +1,115 @@
 from django.shortcuts import render
+from rest_framework import generics, status, views, permissions
+from rest_framework.response import Response
+from .serializers import CustomUserSerializer, EmailVerificationSerializer, LoginSerializer, LogoutSerializer, ResetPasswordEmailRequestSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
-from django.http.response import JsonResponse
-from rest_framework.parsers import JSONParser 
-from rest_framework import status
-from django.contrib.sites.shortcuts import get_current_site
 from .models import CustomUser
-from .serializers import *
-from django.contrib import messages
-from django.core.mail import EmailMessage
-from rest_framework.decorators import api_view
-from django.utils.encoding import force_bytes, force_text, DjangoUnicodeDecodeError
+from .utils import Util
+from django.contrib.sites.shortcuts import get_current_site
 from django.urls import reverse
-from .utils import account_activation_token
+import jwt
+from django.conf import settings
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+from .renderers import CustomUserRenderer
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils.encoding import smart_str, force_str, smart_bytes, DjangoUnicodeDecodeError
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.shortcuts import redirect
+from django.http import HttpResponsePermanentRedirect
+
+class RegisterView(generics.GenericAPIView):
+
+    serializer_class = CustomUserSerializer
+    renderer_classes = (CustomUserRenderer,)
+
+    def post(self, request):
+        user = request.data
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        user_data = serializer.data
+        user = CustomUser.objects.get(email=user_data['email'])
+        token = RefreshToken.for_user(user).access_token
+        current_site = get_current_site(request).domain
+        relativeLink = reverse('email-verify')
+        absurl = 'http://'+current_site+relativeLink+"?token="+str(token)
+        email_body = 'Hola '+user.nombre_usuario + \
+            ' Verica tu cuenta con el siguiente link \n' + absurl
+        data = {'email_body': email_body, 'to_email': user.email,
+                'email_subject': 'Verifica tu email'}
+
+        Util.send_email(data)
+        return Response(user_data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['POST'])
-def register_user(request):
+class VerifyEmail(views.APIView):
+    serializer_class = EmailVerificationSerializer
 
-      customUser_data = JSONParser().parse(request)
-      customUser_serializer = CustomUserSerializer(data=customUser_data)
-     
-      if customUser_serializer.is_valid():
-        customUser_serializer.is_active = False
-        customUser_serializer.save()
+    token_param_config = openapi.Parameter(
+        'token', in_=openapi.IN_QUERY, description='Description', type=openapi.TYPE_STRING)
 
-  
-        email = CustomUser.objects.get(email=customUser_data['email'])
+    @swagger_auto_schema(manual_parameters=[token_param_config])
+    def get(self, request):
+        token = request.GET.get('token')
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY)
+            user = CustomUser.objects.get(id=payload['user_id'])
+            if not user.is_verified:
+                user.is_verified = True
+                user.save()
+            return Response({'email': 'Cuenta activada'}, status=status.HTTP_200_OK)
+        except jwt.ExpiredSignatureError as identifier:
+            return Response({'error': 'La activación de cuenta ha expirado'}, status=status.HTTP_400_BAD_REQUEST)
+        except jwt.exceptions.DecodeError as identifier:
+            return Response({'error': 'Token inválido'}, status=status.HTTP_400_BAD_REQUEST)
 
-        current_site = get_current_site(request)
-        email_body = {
-                    'user': CustomUser.nombre_usuario,
-                    'domain': current_site.domain,
-                    'id': urlsafe_base64_encode(force_bytes(CustomUser.pk)),
-                    'token': account_activation_token.make_token(CustomUser.token),
-                }
 
-        link = reverse('activate', kwargs={
-                               'id': email_body['uid'], 'token': email_body['token']})
+class LoginAPIView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
 
-        email_subject = 'Activa tu cuenta'
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-        activate_url = 'http://'+current_site.domain+link
+class LogoutAPIView(generics.GenericAPIView):
+     serializer_class = LogoutSerializer
+     permission_classes = (permissions.IsAuthenticated,)
 
-        email = EmailMessage(
-        email_subject,
-        'Hola'+CustomUser.nombre_usuario + ', Por favor, sigue el enlace a continuación para activar tu cuenta \n'+activate_url,
-                    'noreply@jgj.com',
-                    [email],
-        )
-        email.send(fail_silently=False)
-        messages.success(request, 'La cuenta ha sido creda')
-        return render(request, 'authentication/register.html')
-        return JsonResponse(customUser_data, status=status.HTTP_201_CREATED)
+def post(self, request):
+
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+#RESTABLECER PASSWORD
+class RequestPasswordResetEmail(generics.GenericAPIView):
+    serializer_class = ResetPasswordEmailRequestSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        email = request.data.get('email', '')
+
+        if CustomUser.objects.filter(email=email).exists():
+            user = CustomUser.objects.get(email=email)
+            uidb64 = urlsafe_base64_encode(smart_bytes(user.id))
+            token = PasswordResetTokenGenerator().make_token(user)
+            current_site = get_current_site(
+                request=request).domain
+            relativeLink = reverse(
+                'password-reset-confirm', kwargs={'uidb64': uidb64, 'token': token})
+
+            redirect_url = request.data.get('redirect_url', '')
+            absurl = 'http://'+current_site + relativeLink
+            email_body = 'Hola, \n Utiliza este link para restablecer tu contraseña  \n' + \
+                absurl+"?redirect_url="+redirect_url
+            data = {'email_body': email_body, 'to_email': user.email,
+                    'email_subject': 'Reset your passsword'}
+            Util.send_email(data)
+        return Response({'success': 'Te hemos enviado un link para restablecerla'}, status=status.HTTP_200_OK)
 
