6937d414ad833e685ae4dbe15d03e380ecb53662
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-11-16 14:45:54 -0600

    Update models.py

--- knowtured/Categorias_k/models.py
+++ knowtured/Categorias_k/models.py
@@ -3,12 +3,11 @@ from Usuarios_k.models import *
 # Create your models here.
 class Categoria(models.Model):
      
-     usuario = models.ForeignKey(CustomUser, on_delete=models.CASCADE) 
-fecha_creacion=models.DateTimeField("Fecha de registro",auto_now_add=True)
-nombre= models.CharField(null=True, max_length=150)
-descripcion=models.TextField(blank=True)
-img_categorias=models.CharField("Ruta de la imagen", null=True, max_length=150)
-img_extension=models.CharField("Extensión de la imagen", null=True, max_length=150)
+ usuario = models.ForeignKey(CustomUser, on_delete=models.CASCADE) 
+ fecha_creacion=models.DateTimeField("Fecha de registro",auto_now_add=True)
+ nombre= models.CharField(null=True, max_length=150)
+ descripcion=models.TextField(blank=True)
+ img_categorias=models.ImageField(upload_to='images/categorias/')
 			
 def __str__(self):
 		return self.nombre
