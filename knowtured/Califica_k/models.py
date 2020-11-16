4216daf59618e5cacc8f6e7392bbc4628f176b06
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-11-16 15:01:28 -0600

    Update models.py

--- knowtured/Califica_k/models.py
+++ knowtured/Califica_k/models.py
@@ -6,8 +6,8 @@ class Califica(models.Model):
      
      usuario_alumno = models.ForeignKey(CustomUser,on_delete=models.CASCADE, related_name="usuario_alumno") 
      usuario_tutor = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="usuario_tutor") 
-     calificacion=models.IntegerField()
-fecha_hora=models.DateTimeField('Registro de hora',auto_now_add=True)
+     calificacion=models.DecimalField()
+     fecha_hora=models.DateTimeField('Registro de hora',auto_now_add=True)
 			
 def __str__(self):
 		return self.calificacion
