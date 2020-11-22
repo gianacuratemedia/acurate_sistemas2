81bf9822401066d27c76a51754d36afadb28d4bf
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-11-21 18:19:37 -0600

    Update serializers.py

--- knowtured/Usuarios_k/serializers.py
+++ knowtured/Usuarios_k/serializers.py
@@ -1,7 +1,10 @@
+
+
+
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from .models import CustomUser
-from validate_email import validate_email
+#from validate_email import validate_email
 from django.contrib import auth
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework_simplejwt.tokens import RefreshToken, TokenError
@@ -9,23 +12,51 @@ from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.utils.encoding import smart_str, force_str, smart_bytes, DjangoUnicodeDecodeError
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
+
+# Django
+from django.contrib.auth import password_validation, authenticate
+
 #Registrar usuario
-class CustomUserSerializer(serializers.ModelSerializer):
-    nombre_usuario = serializers.CharField(max_lemin_length=6)
+"""class CustomUserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=55, min_length=8)
     email = serializers.EmailField(
             required=True,
-            validators=[UniqueValidator(queryset=CustomUser.objects.all()), validate_email]
+            validators=[UniqueValidator(queryset=CustomUser.objects.all())]
             )
-    password = serializers.CharField(max_lemin_length=6)
+    password = serializers.CharField(max_length=55, min_length=8)
 
     class Meta:
         model = CustomUser
-        fields = ('pk','nombre_usuario', 'email', 'password')
+        fields = ('username', 'email', 'password')
 
     def create(self, validated_data):
-        customUser = CustomUser.objects.create_user(validated_data['nombre_usuario'],validated_data['email'],
+        customUser = CustomUser.objects.create_user(validated_data['username'],validated_data['email'],
              validated_data['password'])
-        return customUser
+        return customUser"""
+#REGISTRO DE USUARIO
+        
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        max_length=68, min_length=6, write_only=True)
+
+    default_error_messages = {
+        'username': 'El nombre de usuario solo debe contener caracteres alfanuméricos'}
+
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'username', 'password']
+
+    def validate(self, attrs):
+        email = attrs.get('email', '')
+        username = attrs.get('username', '')
+
+        if not username.isalnum():
+            raise serializers.ValidationError(
+                self.default_error_messages)
+        return attrs
+
+    def create(self, validated_data):
+        return CustomUser.objects.create_user(**validated_data)
 
     #Verificacion de cuenta:
 class EmailVerificationSerializer(serializers.ModelSerializer):
@@ -36,52 +67,47 @@ class EmailVerificationSerializer(serializers.ModelSerializer):
 
 #Login
 class LoginSerializer(serializers.ModelSerializer):
-    nombre_usuario = serializers.CharField(
-        max_length=255, min_length=3, read_only=True)
+
     email = serializers.EmailField(max_length=255, min_length=3)
     password = serializers.CharField(
         max_length=68, min_length=6, write_only=True)
-    
+    username = serializers.CharField(
+        max_length=255, min_length=3, read_only=True)
 
     tokens = serializers.SerializerMethodField()
 
     def get_tokens(self, obj):
-        customUser = CustomUser.objects.get(email=obj['email'])
+        customuser = CustomUser.objects.get(email=obj['email'])
 
         return {
-            'refresh': customUser.tokens()['refresh'],
-            'access': customUser.tokens()['access']
+            'refresh': customuser.tokens()['refresh'],
+            'access': customuser.tokens()['access']
         }
 
     class Meta:
         model = CustomUser
-        fields = ['nombre_usuario','email', 'password', 'tokens']
+        fields = ['email', 'password', 'username', 'tokens']
 
     def validate(self, attrs):
-         email = attrs.get('email', '')
-         password = attrs.get('password', '')
-         filtered_user_by_email = CustomUser.objects.filter(email=email)
-         customUser = auth.authenticate(email=email, password=password)
+        email = attrs.get('email','')
+        password = attrs.get('password','')
+        filtered_user_by_email = CustomUser.objects.filter(email=email)
 
-         if filtered_user_by_email.exists() and filtered_user_by_email[0].auth_provider != 'email':
-            raise AuthenticationFailed(
-                detail='Continúe con su inicio de sesión usando ' + filtered_user_by_email[0].auth_provider)
+        customuser = authenticate(email=email, password=password)
 
-         if not customUser:
-            raise AuthenticationFailed('Credenciales inválidas, intentelo de nuevo')
-         if not customUser.is_active:
-            raise AuthenticationFailed('Cuenta inhabilitada, contacta con el administrador')
-         if not customUser.is_verified:
+        if not customuser:
+            raise AuthenticationFailed('Error de email/contraseña, intentalo de nuevo')
+        if not customuser.is_active:
+            raise AuthenticationFailed('Cuenta inhabilitada')
+        if not customuser.is_verified:
             raise AuthenticationFailed('Email no verificado')
-
-         return {
-            'email': customUser.email,
-            'nombre_usuario': customUser.nombre_usuario,
-            'tokens': customUser.tokens
+        return {
+            'email': customuser.email,
+            'username': customuser.username,
+            'tokens': customuser.tokens
         }
-      
-        
-         return super().validate(attrs)
+
+        return super().validate(attrs)
          
 #Cerrar sesión
 
@@ -113,30 +139,3 @@ class ResetPasswordEmailRequestSerializer(serializers.Serializer):
 
     class Meta:
         fields = ['email']
-
-
-#Login
-class LoginSerializer(serializers.ModelSerializer):
-    nombre_usuario = serializers.CharField(
-        max_length=255, min_length=3, read_only=True)
-    email = serializers.EmailField(max_length=255, min_length=3)
-    password = serializers.CharField(
-        max_length=68, min_length=6, write_only=True)
-    
-
-    tokens = serializers.SerializerMethodField()
-
-    def get_tokens(self, obj):
-        customUser = CustomUser.objects.get(email=obj['email'])
-
-        return {
-            'refresh': customUser.tokens()['refresh'],
-            'access': customUser.tokens()['access']
-        }
-
-    class Meta:
-        model = CustomUser
-        fields = ['nombre_usuario','email', 'password', 'tokens']
-
-
-
