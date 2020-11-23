abf7bca59b8f3d24fd9483e94771254adcdae947
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-11-23 14:57:36 -0600

    Update models.py

--- knowtured/Categorias_k/models.py
+++ knowtured/Categorias_k/models.py
@@ -1,13 +1,18 @@
 from django.db import models
-from Usuarios_k.models import *
+from Usuarios_k.models import User
+
+
 # Create your models here.
+
+
 class Categoria(models.Model):
-     
- usuario = models.ForeignKey(CustomUser, on_delete=models.CASCADE) 
- fecha_creacion=models.DateTimeField("Fecha de registro",auto_now_add=True)
- nombre= models.CharField(null=True, max_length=150)
- descripcion=models.TextField(blank=True)
- img_categorias=models.ImageField(upload_to='images/categorias/')
-			
-def __str__(self):
-		return self.nombre
+
+    nombre= models.CharField(null=True, max_length=150)
+    descripcion=models.TextField(blank=True)
+    img_categoria=models.ImageField(null=True, upload_to='images/categorias/')
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering: ['-updated_at']
