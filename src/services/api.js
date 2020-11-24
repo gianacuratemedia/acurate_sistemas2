9416e7f0385ae39e12c70d17aa4c7fef90d1a33a
JavierPinedoC <a01551344@itesm.mx>
2020-11-23 22:44:31 -0600

    Instalar Axios y configuración

---
+++ src/services/api.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "http://104.198.244.0:8123/",
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  },
+});
