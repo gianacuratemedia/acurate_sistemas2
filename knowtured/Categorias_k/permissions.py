ec71bd551af8dd4b2df55c2116ab15f36fe3ca80
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-11-24 19:31:36 -0600

    Update permissions.py

--- knowtured/Categorias_k/permissions.py
+++ knowtured/Categorias_k/permissions.py
@@ -5,3 +5,7 @@ class IsOwner(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return obj.owner == request.user
+
+class PublicEndpoint(permissions.BasePermission):
+       def has_permission(self, request, view):
+           return True
