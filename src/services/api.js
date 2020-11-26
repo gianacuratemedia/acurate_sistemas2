5f1e9774392e06440ce7722cd43d4284438d914d
JavierPinedoC <a01551344@itesm.mx>
2020-11-26 17:15:30 -0600

    API original

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://104.198.244.0:8123/",
+  // baseURL: "http://104.198.244.0:8123/",
+  baseURL: "http://165.227.62.217/",
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
