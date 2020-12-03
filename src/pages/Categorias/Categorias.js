bf0f31a2eb5435a9df955fa26c389133d5939cfe
JavierPinedoC <a01551344@itesm.mx>
2020-12-02 18:37:54 -0600

    conexión a categorias en la API

--- src/pages/Categorias/Categorias.js
+++ src/pages/Categorias/Categorias.js
@@ -1,102 +1,97 @@
-import React, {useState} from 'react';
-
+import React, { useState, useEffect } from "react";
 
 import imagenes from "../../assets/img/imagenes";
 
-import './Category.css';
-
-
-function handleSummit(){
-
-}
-
+import "./Category.css";
+import API from "../../services/api";
+function handleSummit() {}
 
 const Categorias = () => {
+  useEffect(() => {
+    const response = API.get(`categorias/`).then((res) => {
+      console.log(res);
+    });
+  });
+  return (
+    <div className="catcont">
+      <img className="fondo" src={imagenes["logoazul2"]} />
 
-    return (
-        
-        <div className="catcont">
-          <img className="fondo"  src={imagenes["logoazul2"]} />
+      <div className="title">
+        <h5 className="titlewhite">Selecciona</h5>
+        <h5 className="titleblue">3 Categorias</h5>
+      </div>
 
-            <div className="title">
-                <h5 className="titlewhite">Selecciona</h5><h5 className="titleblue">3 Categorias</h5>
-            </div>
-
-
-               <div className="contenedor">
-                   <div className="item">
-                    <a href="#Example"> 
-                     <img className="icon"  src={imagenes["imgpersonal"]}  /> 
-                     </a>
-                      </div>
-                   
-                        <div className="item">
-                          <a href="">
-                           <img className="icon" src={imagenes["imgpersonal"]} />  
-                           </a>
-                         </div>
-
-                                <div className="item">
-                                  < img onClick={handleSummit} className="icon" src={imagenes["imgpersonal"] }/>  
-                                
-                                 </div>
-
-                                      <div className="item">
-                                           <img className="icon" src={imagenes["imgpersonal"]} />  
-                                      </div>
-
-                                              <div className="item">
-                                                 <img className="icon" src={imagenes["imgpersonal"]} />  
-                                                </div>
-                   
-                                                   <div className="item">
-                                                           <img className="icon" src={imagenes["imgpersonal"]} />  
-                                                          </div>
-                   
-                                                               <div className="item">
-                                                                   <img className="icon" src={imagenes["imgpersonal"]} />  
-                                                                 </div>
-
-                                                                      <div className="item">
-                                                                          <img className="icon" src={imagenes["imgpersonal"]} />  
-                                                                       </div>
-
-                                                                                <div className="item">
-                                                                                     <img className="icon" src={imagenes["imgpersonal"]} />  
-                                                                                  </div>
-
-                                                                                           <div className="item">
-                                                                                             <img className="icon" src={imagenes["imgpersonal"]} />  
-                                                                                                </div>
-
-                                                                                                    <div className="item">
-                                                                                                       <img className="icon" src={imagenes["imgpersonal"]} />  
-                                                                                                       </div>
-
-                                                                                                          <div className="item">
-                                                                                                              <img className="icon" src={imagenes["imgpersonal"]} />  
-                                                                                                          </div>
-               </div>
-
-                        <li>
-                      <a href="" className="button-saltar">
-                            Saltar
-                           </a>
-                       </li>
-
-
-
-                <li>
-                <a href="" className="button-comenzar">
-                   ¡Comencemos!
-                  </a>
-                </li>
+      <div className="contenedor">
+        <div className="item">
+          <a href="#Example">
+            <img className="icon" src={imagenes["imgpersonal"]} />
+          </a>
         </div>
 
+        <div className="item">
+          <a href="">
+            <img className="icon" src={imagenes["imgpersonal"]} />
+          </a>
+        </div>
 
+        <div className="item">
+          <img
+            onClick={handleSummit}
+            className="icon"
+            src={imagenes["imgpersonal"]}
+          />
+        </div>
 
-    );
+        <div className="item">
+          <img className="icon" src={imagenes["imgpersonal"]} />
+        </div>
 
-}
+        <div className="item">
+          <img className="icon" src={imagenes["imgpersonal"]} />
+        </div>
+
+        <div className="item">
+          <img className="icon" src={imagenes["imgpersonal"]} />
+        </div>
+
+        <div className="item">
+          <img className="icon" src={imagenes["imgpersonal"]} />
+        </div>
+
+        <div className="item">
+          <img className="icon" src={imagenes["imgpersonal"]} />
+        </div>
+
+        <div className="item">
+          <img className="icon" src={imagenes["imgpersonal"]} />
+        </div>
+
+        <div className="item">
+          <img className="icon" src={imagenes["imgpersonal"]} />
+        </div>
+
+        <div className="item">
+          <img className="icon" src={imagenes["imgpersonal"]} />
+        </div>
+
+        <div className="item">
+          <img className="icon" src={imagenes["imgpersonal"]} />
+        </div>
+      </div>
+
+      <li>
+        <a href="" className="button-saltar">
+          Saltar
+        </a>
+      </li>
+
+      <li>
+        <a href="" className="button-comenzar">
+          ¡Comencemos!
+        </a>
+      </li>
+    </div>
+  );
+};
 
 export default Categorias;
