bdc66e492edb63b20a70cd28f8f44cccdfadf2a7
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-12-04 15:57:34 -0600

    Update views.py

--- knowtured/Realiza_k/views.py
+++ knowtured/Realiza_k/views.py
@@ -1,54 +1,138 @@
-from django.shortcuts import render
-
-from django.http.response import JsonResponse
-from rest_framework.parsers import JSONParser 
 from rest_framework import status
- 
+from rest_framework.response import Response
+from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .serializers import RealizaSerializer_alumno, RealizaSerializer_tutor
 from .models import Realiza
-from .serializers import *
-from rest_framework.decorators import api_view
+from Usuarios_k.models import User
+from rest_framework import permissions
+from .permissions import IsOwner
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import permission_classes
 
-@api_view(['GET', 'POST', 'DELETE'])
-def realiza_list(request):
-    # GET lista de tareas a realizar, POST subir una tarea, DELETE alguna tarea
-  if request.method == 'GET':
-        realizas = Realiza.objects.all()
-        
-        title = request.GET.get('title', None)
-        if title is not None:
-            realizas = realizas.filter(title__icontains=title)
-        
-        realizas_serializer = RealizaSerializer(realizas, many=True)
-        return JsonResponse(realizas_serializer.data, safe=False)
-       
-  elif request.method == 'POST':
-         
-      realiza_data = JSONParser().parse(request)
-      realiza_serializer = RealizaSerializer(data=realiza_data)
-      if realiza_serializer.is_valid():
-        realiza_serializer.save()
-        return JsonResponse(realiza_serializer.data, status=status.HTTP_201_CREATED) 
-        return JsonResponse(realiza_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
- 
-      elif request.method == 'DELETE':
-        count = Realiza.objects.all().delete()
-        return JsonResponse({'message': '{} Todos los registros han sido eliminados'.format(count[0])}, status=status.HTTP_204_NO_CONTENT)
-@api_view(['GET', 'PUT', 'DELETE'])
-def realiza_detail(request, pk):
-    # find realiza by pk (id)
-    try: 
-        realiza = Realiza.objects.get(pk=pk) 
-    except Realiza.DoesNotExist: 
-        return JsonResponse({'message': 'Registro de tarea y/o actividad no encontrado'}, status=status.HTTP_404_NOT_FOUND) 
- 
-    # GET / PUT / DELETE realiza
+#Lista de las actividades realizadas, solo por usuario (alumno)
+
+class RealizaList(ListCreateAPIView):
+    serializer_class = RealizaSerializer_alumno
+    queryset = Realiza.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
+    def get_queryset(self):
+        return self.queryset.filter(alumno_id=self.request.user)
+
+    # Post realiza 
+    def post(self, request):
+        serializer = RealizaSerializer_alumno(data=request.data)
+        if serializer.is_valid():
+            serializer.save(alumno_id=self.request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RealizaDetail(RetrieveUpdateDestroyAPIView):
+    serializer_class = RealizaSerializer_alumno
+    permission_classes = (permissions.IsAuthenticated, IsOwner,)
     
+    def get_queryset(self,id):
+        try:
+            realiza = Realiza.objects.get(id=id)
+        except Realiza.DoesNotExist:
+            content = {
+                'status': 'Not Found'
+            }
+            return Response(content, status=status.HTTP_404_NOT_FOUND)
+        return realiza
+
+    # Get realiza
+    def get(self, request, id):
+
+        realiza = self.get_queryset(id)
+        serializer = RealizaSerializer_alumno(realiza)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # Update realiza
+    def put(self, request, id):
         
-@api_view(['GET'])
-def realiza_list_published(request):
-     # GET todas las tareas que el usuario tiene
-    realizas = Realiza.objects.filter(published=True)
+        realiza = self.get_queryset(id)
+
+        if(request.user == realiza.alumno_id): # Si el alumno en quien hace el request
+            serializer = RealizaSerializer_alumno(realiza, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            content = {
+                'status': 'UNAUTHORIZED'
+            }
+            return Response(content, status=status.HTTP_401_UNAUTHORIZED)
+
+    # Delete 
+    def delete(self, request, id):
+
+        realiza = self.get_queryset(id)
+
+        if(request.user == realiza.alumno_id): # Si el alumno es quien hace el request
+            realiza.delete()
+            content = {
+                'status': 'NO CONTENT'
+            }
+            return Response(content, status=status.HTTP_204_NO_CONTENT)
+        else:
+            content = {
+                'status': 'UNAUTHORIZED'
+            }
+            return Response(content, status=status.HTTP_401_UNAUTHORIZED)
+
+
+#Tutor asignando calificacion
+#Lista de calificaciones
+class AsignarCalificaciones(ListCreateAPIView):
+   serializer_class = RealizaSerializer_tutor
+   permission_classes = (permissions.IsAuthenticated,)
+   
+   def get_queryset(self,contenido_id):
+        try:
+            realiza = Realiza.objects.get(contenido_id=contenido_id)
+        except Realiza.DoesNotExist:
+            content = {
+                'status': 'Not Found'
+            }
+            return Response(content, status=status.HTTP_404_NOT_FOUND)
+        return realiza
+
+
+class RealizaUpdateC(RetrieveUpdateDestroyAPIView):
+    serializer_class = RealizaSerializer_tutor
+    permission_classes = (permissions.IsAuthenticated,)
+    
+    def get_queryset(self,id):
+        try:
+            realiza = Realiza.objects.get(id=id)
+        except Realiza.DoesNotExist:
+            content = {
+                'status': 'Not Found'
+            }
+            return Response(content, status=status.HTTP_404_NOT_FOUND)
+        return realiza
+
+    # Get realiza
+    def get(self, request, id):
+
+        realiza = self.get_queryset(id)
+        serializer = RealizaSerializer_tutor(realiza)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # Update realiza
+    def put(self, request, id):
         
-    if request.method == 'GET': 
-        realizas_serializer = RealizaSerializer(realizas, many=True)
-        return JsonResponse(realizas_serializer.data, safe=False)
+        realiza = self.get_queryset(id)
+        serializer = RealizaSerializer_tutor(realiza, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            content = {
+                'status': 'UNAUTHORIZED'
+            }
+            return Response(content, status=status.HTTP_401_UNAUTHORIZED)
+
