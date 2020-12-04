aa02467fe3b17f917ff5dab5fbcf0ee1eed9bd44
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-12-04 15:51:33 -0600

    Update models.py

--- knowtured/Gana_k/models.py
+++ knowtured/Gana_k/models.py
@@ -4,9 +4,14 @@ from Logros_k.models import *
 # Create your models here.
 class Gana(models.Model):
      
-     id_logro= models.ForeignKey(Logro, on_delete=models.CASCADE, related_name="id_logro") 
-     usuario = models.ForeignKey(CustomUser,on_delete=models.CASCADE) 
+     id_logro= models.ForeignKey(to=Logro, on_delete=models.CASCADE, related_name="id_logro") 
+     owner = models.ForeignKey(to=User,on_delete=models.CASCADE) 
      fecha_hora=models.DateTimeField('Registro de hora',auto_now_add=True)
-			
-def __str__(self):
-		return self.id_logro
+     created_at = models.DateTimeField(auto_now_add=True)
+     updated_at = models.DateTimeField(auto_now=True)
+
+     class Meta:
+        ordering: ['-updated_at']
+
+     def __str__(self):
+        return str(self.owner)+'s logro'
