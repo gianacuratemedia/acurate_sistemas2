44e35ea2c0a07a0a8a6d8db1656d81abea414ddd
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-12-04 15:53:22 -0600

    Update models.py

--- knowtured/Inscribe_k/models.py
+++ knowtured/Inscribe_k/models.py
@@ -3,12 +3,11 @@ from Usuarios_k.models import *
 from Curso_k.models import *
 
 # Create your models here.
-class Inscribe(models.Model):
-     
-     usuario_id = models.ForeignKey(CustomUser,on_delete=models.CASCADE) 
-     curso_id = models.ForeignKey(Curso,on_delete=models.CASCADE) 
+class Inscripcion(models.Model):
+     curso_id = models.ForeignKey(Curso,on_delete=models.CASCADE)
+     alumno_id = models.ForeignKey(to=User,on_delete=models.CASCADE)  
      fecha_inscripcion=models.DateTimeField('Fecha de inscripción:',auto_now_add=True)
-     promedio=models.DecimalField(null=True)
+     promedio=models.DecimalField(null=True, decimal_places=4, max_digits=10)
     
 
 	
