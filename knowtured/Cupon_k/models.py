022b9904123c43ac96cef106ae698e920a111bc8
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-12-04 15:37:24 -0600

    Update models.py

--- knowtured/Cupon_k/models.py
+++ knowtured/Cupon_k/models.py
@@ -2,13 +2,15 @@ from django.db import models
 from Usuarios_k.models import *
 # Create your models here.
 class Cupon(models.Model):
-     
-     usuario = models.ForeignKey(CustomUser, on_delete=models.CASCADE) 
-fecha_creacion=models.DateTimeField("Fecha de registro",auto_now_add=True)
-cantidad_existencia=models.IntegerField()
-clave_promo= models.CharField(null=True, max_length=150)
-descuento=models.DecimalField()
+  owner= models.ForeignKey(to=User, on_delete=models.CASCADE) 
+  cantidad_existencia=models.IntegerField()
+  clave_promo= models.CharField(null=True, max_length=150)
+  descuento=models.FloatField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+   
+  class Meta:
+        ordering: ['-updated_at']
 
-			
-def __str__(self):
-		return self.clave_promo
+  def __str__(self):
+        return str(self.clave_promo)
