7d636e21e4075fc4d2c7c4b114842ca95e1a1175
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-12-04 16:00:53 -0600

    Update models.py

--- knowtured/Sigue_k/models.py
+++ knowtured/Sigue_k/models.py
@@ -5,11 +5,14 @@ from django.core.exceptions import ValidationError
 # Create your models here.
 class Sigue(models.Model):
          
-     usuario_alumno = models.ForeignKey(CustomUser,on_delete=models.CASCADE, related_name="usuario_alumno") 
-     usuario_tutor = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="usuario_tutor") 
+     usuario_tutor = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_tutor") 
+     usuario_alumno = models.ForeignKey(to=User,on_delete=models.CASCADE, related_name='%(class)s_requests_created') 
      fecha_hora=models.DateTimeField('Registro de hora',auto_now_add=True)
-			
-     def save(self, *args, **kwargs):
+
+def __str__(self):
+         return str(self.usuario_tutor)
+
+def save(self, *args, **kwargs):
         # Revisar campos duplicados
         try:
             sigue = Sigue.objects.get(usuario_alumno=self.usuario_alumno, usuario_tutor=self.usuario_tutor)
@@ -23,5 +26,4 @@ class Sigue(models.Model):
             raise NotImplementedError('Registros duplicados', code='invalid')
         except self.DoesNotExist:
             super().save(*args, **kwargs)
-def __str__(self):
-		return self.usuario_alumno
+            
