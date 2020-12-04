8d5bb2cc73287a673533fc44c86b61181bca0eb8
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-12-04 15:56:57 -0600

    Update models.py

--- knowtured/Realiza_k/models.py
+++ knowtured/Realiza_k/models.py
@@ -1,20 +1,20 @@
 from django.db import models
 from Usuarios_k.models import *
-from Contenido_k.models import *
+from Curso_k.models import Contenido
 # Create your models here.
 
 class Realiza(models.Model):
      
-     usuario_id = models.ForeignKey(CustomUser,on_delete=models.CASCADE) 
-     contenido_id = models.ForeignKey(Contenido,on_delete=models.CASCADE) 
+     alumno_id = models.ForeignKey(to=User,on_delete=models.CASCADE) 
+     contenido_id = models.ForeignKey(to=Contenido,on_delete=models.CASCADE) 
      fecha_hora=models.DateTimeField('Registro de hora',auto_now_add=True)
-     calificacion=models.DecimalField(null=True)
+     calificacion=models.FloatField(null=True)
      archivo=models.FileField(upload_to='Recursos/Tareas', null=True, blank= True)
 
      def save(self, *args, **kwargs):
         # Revisar campos duplicados
         try:
-            realiza = Realiza.objects.get(usuario_id=self.usuario_id, contenido_id=self.contenido_id)
+            realiza = Realiza.objects.get(alumno_id=self.alumno_id, contenido_id=self.contenido_id)
             raise NotImplementedError('Registros duplicados', code='invalid')
         except self.DoesNotExist:
             super().save(*args, **kwargs)
