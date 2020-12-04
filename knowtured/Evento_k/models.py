267704606b4cdba2d6a35ab152f507f6af7abb6a
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-12-04 15:46:29 -0600

    Update models.py

--- knowtured/Evento_k/models.py
+++ knowtured/Evento_k/models.py
@@ -3,10 +3,15 @@ from Usuarios_k.models import *
 # Create your models here.
 class Evento(models.Model):
      
-     usuario_id = models.ForeignKey(CustomUser,on_delete=models.CASCADE) 
-     nota=models.CharField(max_length=500, null=False)
-     fecha_hora=models.DateTimeField('Registro de hora',auto_now_add=True)
+     owner = models.ForeignKey(to=User,on_delete=models.CASCADE) 
+     nota=models.CharField(max_length=600, null=False)
+     fecha_hora=models.DateField('Fecha y hora del evento',null=True)
      nombre=models.CharField(max_length=300, null=False)
-			
-def __str__(self):
-		return self.nombre
+     created_at = models.DateTimeField(auto_now_add=True)
+     updated_at = models.DateTimeField(auto_now=True)
+
+     class Meta:
+        ordering: ['-updated_at']
+
+     def __str__(self):
+        return str(self.owner)+'s event'	
