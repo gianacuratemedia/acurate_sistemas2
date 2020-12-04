a780aabe0b148af54c52b423802062d5bc1fd63a
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-12-04 15:55:04 -0600

    Update models.py

--- knowtured/Logros_k/models.py
+++ knowtured/Logros_k/models.py
@@ -4,13 +4,16 @@ from Usuarios_k.models import *
 # Create your models here.
 class Logro(models.Model):
      
-     usuario = models.ForeignKey(CustomUser, on_delete=models.CASCADE) 
-     fecha_creacion=models.DateTimeField("Fecha de creación",auto_now_add=True)
+     owner = models.ForeignKey(to=User, on_delete=models.CASCADE) 
      nombre= models.CharField(null=True, max_length=150)
      nivel_desbloqueo=models.IntegerField()
      descripcion= models.CharField(null=True, max_length=150)
-     img_ubicacion= models.CharField(null=True, max_length=500)
-     img_extension= models.CharField(null=True, max_length=10)
-	
-def __str__(self):
-		return self.nombre
+     img_logro=models.ImageField(null=True, upload_to='images/logros/')
+     created_at = models.DateTimeField(auto_now_add=True)
+     updated_at = models.DateTimeField(auto_now=True)
+
+     class Meta:
+        ordering: ['-updated_at']
+
+     def __str__(self):
+        return str(self.nombre)	
