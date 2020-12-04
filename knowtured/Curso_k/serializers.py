e76bab5f7054d05d9f6e7add02fbcb14d3d212f4
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-12-04 15:42:27 -0600

    Update serializers.py

--- knowtured/Curso_k/serializers.py
+++ knowtured/Curso_k/serializers.py
@@ -1,16 +1,31 @@
 from rest_framework import serializers
-from .models import Curso
+from .models import Curso, Contenido
 
 
 class CursoSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Curso
         fields = ['id', 'categoria_id','nombre', 'img_curso', 'premium', 'duracion','dificultad','limite','descripcion']
-
-
+    
+    
 class CursoSerializerP(serializers.ModelSerializer):
 
     class Meta:
         model = Curso
         fields = ['nombre', 'img_curso', 'premium', 'duracion','dificultad','limite','descripcion']
+
+
+
+
+class ContenidoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Contenido
+        fields = ['id', 'curso_id', 'tipo', 'mensaje','archivo','fecha_limite']
+
+
+class PublicContenidoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Contenido
+        fields = ['curso_id', 'tipo', 'mensaje','archivo','fecha_limite']
