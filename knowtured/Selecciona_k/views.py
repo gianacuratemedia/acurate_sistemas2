a67e5d25dbf232277376aa7db1b07cc37651c76f
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-12-04 15:59:36 -0600

    Update views.py

--- knowtured/Selecciona_k/views.py
+++ knowtured/Selecciona_k/views.py
@@ -1,69 +1,72 @@
-from django.shortcuts import render
-
-from django.http.response import JsonResponse
-from rest_framework.parsers import JSONParser 
 from rest_framework import status
- 
+from rest_framework.response import Response
+from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .serializers import SeleccionaSerializer
 from .models import Selecciona
-from .serializers import *
-from rest_framework.decorators import api_view
+from Usuarios_k.models import User
+from rest_framework import permissions
+from .permissions import IsOwner
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import permission_classes
 
-@api_view(['GET', 'POST', 'DELETE'])
-def selecciona_list(request):
-    # GET lista de categorías, POST seguir una categoría nueva, DELETE una de las categorías seleccionadas
-  if request.method == 'GET':
-        seleccionas = Selecciona.objects.all()
-        
-        title = request.GET.get('title', None)
-        if title is not None:
-            seleccionas = seleccionas.filter(title__icontains=title)
-        
-        seleccionas_serializer = SeleccionaSerializer(seleccionas, many=True)
-        return JsonResponse(seleccionas_serializer.data, safe=False)
-       
-  elif request.method == 'POST':
-         
-      selecciona_data = JSONParser().parse(request)
-      selecciona_serializer = SeleccionaSerializer(data=selecciona_data)
-      if selecciona_serializer.is_valid():
-        selecciona_serializer.save()
-        return JsonResponse(selecciona_serializer.data, status=status.HTTP_201_CREATED) 
-        return JsonResponse(selecciona_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
- 
-      elif request.method == 'DELETE':
-        count = Selecciona.objects.all().delete()
-        return JsonResponse({'message': '{} Las categorías a las que sigues han sido eliminadas'.format(count[0])}, status=status.HTTP_204_NO_CONTENT)
-@api_view(['GET', 'PUT', 'DELETE'])
-def selecciona_detail(request, pk):
-    # find selecciona by pk (id)
-    try: 
-        selecciona = Selecciona.objects.get(pk=pk) 
-    except Selecciona.DoesNotExist: 
-        return JsonResponse({'message': 'No sigue a la categoría seleccionada'}, status=status.HTTP_404_NOT_FOUND) 
- 
-    # GET / PUT / DELETE selección
+#Lista de categorias que sigue un usuario
 
-    if request.method == 'GET': 
-         selecciona_serializer = SeleccionaSerializer(selecciona) 
-         return JsonResponse(selecciona_serializer.data) 
-        
-    elif request.method == 'PUT': 
-        selecciona_data = JSONParser().parse(request) 
-        selecciona_serializer = SeleccionaSerializer(selecciona, data=selecciona_data) 
-        if selecciona_serializer.is_valid(): 
-            selecciona_serializer.save() 
-            return JsonResponse(selecciona_serializer.data) 
-        return JsonResponse(selecciona_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
- 
-    elif request.method == 'DELETE': 
-        selecciona.delete() 
-        return JsonResponse({'message': 'Has eliminado la categoría a la que sigues'}, status=status.HTTP_204_NO_CONTENT)
-@api_view(['GET'])
-def selecciona_list_published(request):
-     # GET todas las categorías que sigues
-    seleccionas = Selecciona.objects.filter(published=True)
-        
-    if request.method == 'GET': 
-        seleccionas_serializer = SeleccionaSerializer(seleccionas, many=True)
-        return JsonResponse(seleccionas_serializer.data, safe=False)
+class SeleccionaList(ListCreateAPIView):
+    serializer_class = SeleccionaSerializer
+    queryset = Selecciona.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
+    def get_queryset(self):
+        return self.queryset.filter(owner=self.request.user)
+
+    # Post selecciona una categoria
+    def post(self, request):
+        serializer = SeleccionaSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(owner=self.request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SeleccionaDetail(RetrieveUpdateDestroyAPIView):
+    serializer_class = SeleccionaSerializer
+
+    permission_classes = (permissions.IsAuthenticated, IsOwner,)
+    
+    def get_queryset(self,id):
+        try:
+            selecciona = Selecciona.objects.get(id=id)
+        except Selecciona.DoesNotExist:
+            content = {
+                'status': 'Not Found'
+            }
+            return Response(content, status=status.HTTP_404_NOT_FOUND)
+        return selecciona
+
+    # Get categoria que el alumno sigue por id
+    def get(self, request, id):
+
+        selecciona = self.get_queryset(id)
+        serializer = SeleccionaSerializer(selecciona)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # Delete dejar de seguir una categoria
+    def delete(self, request, id):
+
+        seleciona = self.get_queryset(id)
+
+        if(request.user == seleciona.owner): # Si owner es quien hace el request
+            seleciona.delete()
+            content = {
+                'status': 'NO CONTENT'
+            }
+            return Response(content, status=status.HTTP_204_NO_CONTENT)
+        else:
+            content = {
+                'status': 'UNAUTHORIZED'
+            }
+            return Response(content, status=status.HTTP_401_UNAUTHORIZED)
+   
+
+
          
