89419e383fbb442ff888946c7ec94dac848e7c10
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-12-04 15:47:55 -0600

    Update models.py

--- knowtured/Experiencia_k/models.py
+++ knowtured/Experiencia_k/models.py
@@ -3,11 +3,15 @@ from Usuarios_k.models import *
 # Create your models here.
 class Experiencia(models.Model):
      
-     usuario = models.ForeignKey(CustomUser, on_delete=models.CASCADE) 
-     nombre= models.CharField(null=True, max_length=150)
-     fecha_creacion=models.DateTimeField("Fecha de registro",auto_now_add=True)
-     fecha_inicio=models.DateField(blank=True, null=True)
-     fecha_fin=models.DateField(blank=True, null=True)
-	
+   owner = models.ForeignKey(to=User, on_delete=models.CASCADE) 
+   nombre= models.CharField(null=True, max_length=150)
+   fecha_inicio=models.DateField(blank=True, null=True)
+   fecha_fin=models.DateField(blank=True, null=True)
+   created_at = models.DateTimeField(auto_now_add=True)
+   updated_at = models.DateTimeField(auto_now=True)
+
+class Meta:
+        ordering: ['-updated_at']
+
 def __str__(self):
-		return self.nombre
+        return str(self.owner)+'s experience'	
