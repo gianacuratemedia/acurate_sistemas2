c6f3ab9d14d834c43d79e22609726ac3878b1cf9
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-12-04 15:40:56 -0600

    Update urls.py

--- knowtured/Curso_k/urls.py
+++ knowtured/Curso_k/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from .views import CursoDetail, CursoList, CursoListP
-
+from .views import ContenidoDetail
+from .views import ContenidoList, ContenidoListP
 
 urlpatterns = [
     path('all-cursos/', CursoList.as_view(), name="cursos"),
     path('all-cursos/<int:id>', CursoDetail.as_view(), name="curso"),
-    path('', CursoListP.as_view(), name="all_categorias")
-]
+    path('', CursoListP.as_view(), name="all_categorias"),
 
+    #Contenido de los cursos
+    path('all-contenido/', ContenidoList.as_view(), name="contenidos"),
+    path('all-contenido/<int:id>', ContenidoDetail.as_view(), name="contenido-curso"),
+    path('', ContenidoListP.as_view(), name="all_contenido")
+    
+]
