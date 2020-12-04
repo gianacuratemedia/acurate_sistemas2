76079866142c379c7b08cdc1f89652b280c058d4
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-12-04 15:30:17 -0600

    Update models.py
    Modificaciones modelo califica

--- knowtured/Califica_k/models.py
+++ knowtured/Califica_k/models.py
@@ -4,10 +4,25 @@ from Usuarios_k.models import *
 # Create your models here.
 class Califica(models.Model):
      
-     usuario_alumno = models.ForeignKey(CustomUser,on_delete=models.CASCADE, related_name="usuario_alumno") 
-     usuario_tutor = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="usuario_tutor") 
-     calificacion=models.DecimalField()
+     usuario_alumno = models.ForeignKey(to=User,on_delete=models.CASCADE) 
+     usuario_tutor = models.ForeignKey(User, on_delete=models.CASCADE,  related_name='%(class)s_user_tutor') 
+     calificacion=models.IntegerField(null=True)
      fecha_hora=models.DateTimeField('Registro de hora',auto_now_add=True)
-			
+
 def __str__(self):
-		return self.calificacion
+        return str(self.calificacion)
+			
+def save(self, *args, **kwargs):
+        # Revisar campos duplicados
+        try:
+            califica = Califica.objects.get(usuario_alumno=self.usuario_alumno, usuario_tutor=self.usuario_tutor)
+            raise NotImplementedError('Registros duplicados', code='invalid')
+        except self.DoesNotExist:
+            super().save(*args, **kwargs)
+
+        # Campos duplicados inversos
+        try:
+            califica_new = Califica.objects.get(usuario_tutor=self.usuario_alumno, usuario_alumno=self.usuario_tutor)
+            raise NotImplementedError('Registros duplicados', code='invalid')
+        except self.DoesNotExist:
+            super().save(*args, **kwargs)
