c03cfecd922ac44a03c4a646ea4e7b00f5e9fe2c
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-12-04 16:04:14 -0600

    Delete urls.py

--- knowtured/Selecciona_k/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('all-actividades/', RealizaList.as_view(), name="actividades"), #get and post (alumnos)
-    path('all-actividades/<int:id>', RealizaDetail.as_view(), name="actividad"), #Get, update, delete by id (alumnos)
-    path('all-actividades-contenido/', AsignarCalificaciones.as_view(), name="actividades"), #get actividad por contenido_id (tutor)
-    path('all-actividades-contenido/<int:id>', RealizaUpdateC.as_view(), name="actividad") #Update calificacion (tutor)
-]
