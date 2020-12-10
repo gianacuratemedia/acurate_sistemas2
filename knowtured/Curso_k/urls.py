44d6c3e43cbcf29888b8874d0c379bb2b1fe7c31
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-12-09 20:43:51 -0600

    Update urls.py
    Añadiendo parámetros a las views

--- knowtured/Curso_k/urls.py
+++ knowtured/Curso_k/urls.py
@@ -1,16 +1,26 @@
 from django.urls import path
 from .views import CursoDetail, CursoList, CursoListP
 from .views import ContenidoDetail
-from .views import ContenidoList, ContenidoListP
+from .views import ContenidoList, ContenidoListP, Curso_Categoria_List, CursoList_Categoria_GP
 
 urlpatterns = [
+    #Lista de todos los cursos por el usuario que hace la petición get 
     path('all-cursos/', CursoList.as_view(), name="cursos"),
-    path('all-cursos/<int:id>', CursoDetail.as_view(), name="curso"),
-    path('', CursoListP.as_view(), name="all_categorias"),
+    #Get curso por categoria y curso que el usuario ha hecho y post usando el parametro de la categoria
+    path('all-cursos/categoria/<int:categoria_id>', CursoList_Categoria_GP.as_view(), name="curso_categoria"),
+    #Get, update and delete by id del curso and categoria_id
+    path('all-cursos/categoria/<int:categoria_id>/<int:id>', CursoDetail.as_view(), name="curso"),
+    path('', CursoListP.as_view(), name="all_cursos"),
+    #List cursos por categoria sin permisos
+    path('all-cursos/categorias/<int:categoria_id>', Curso_Categoria_List.as_view(), name="curso_categoria_p"),
 
-    #Contenido de los cursos
-    path('all-contenido/', ContenidoList.as_view(), name="contenidos"),
-    path('all-contenido/<int:id>', ContenidoDetail.as_view(), name="contenido-curso"),
+    #Contenido de curso por id_curso
+    # Get y post 
+    # Post con curso_id 
+    path('all-cursos/<int:curso_id>/all-contenido/', ContenidoList.as_view(), name="contenido_por_curso"),
+    #Get, update and delete by curso_id and id_contenido
+    path('all-cursos/<int:curso_id>/all-contenido/<int:id>', ContenidoDetail.as_view(), name="contenido-curso"),
+    #Get contenido filtrado por curso sin permiso
     path('', ContenidoListP.as_view(), name="all_contenido")
     
 ]
