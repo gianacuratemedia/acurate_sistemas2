712776652da8b966df44211784b607c5c8d6b240
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-12-10 00:30:03 -0600

    Update urls.py

--- knowtured/Foro_k/urls.py
+++ knowtured/Foro_k/urls.py
@@ -1,14 +1,21 @@
 from django.urls import path
-from .views import ForoList, ForoDetail
-
+from .views import ForoList, ForoDetail, ForoList_Curso_GP
+from .views import ComentarioList, ComentarioList_Curso_Foro_GP, ComentarioDetail
 
 urlpatterns = [
-#Get mensajes publicados por usuario y post un mensaje en 
-    path('all-tus-mensajes/', ForoList.as_view(), name="mensajes_user"),
+#Get foros creados por el usuario
+    path('tus-foros/', ForoList.as_view(), name="foro_user"),
+#Get and post foro por url del curso
+    path('curso/<int:curso_id>', ForoList_Curso_GP.as_view(), name="foro_curso"),
+ #Get and delete by curso_id and foro_id
+    path('curso/<int:curso_id>/foro/<int:id>', ForoDetail.as_view(), name="foro_detail"),
 
-#Get mensajes o publicaciones por id_curso
-    path('all-cursos/<int:curso_id>', ForoDetail.as_view(), name="mensajes_curso"),
+#Publicaciones o mensajes
+#Get comentarios publicados por el usuario
+    path('tus-comentarios-en-foros/', ComentarioList.as_view(), name="comentarios_user"),
+#Get and post comentarios por url de foro_id
+    path('foro/<foro_id>/comentarios/', ComentarioList_Curso_Foro_GP.as_view(), name="comentarios_curso"),
 #Update and delete mensaje por id del mensaje
-    path('all-cursos/<int:id>', ForoDetail.as_view(), name="mensajes_id-mensaje")
+    path('foro/<foro_id>/comentarios/<int:id>', ComentarioDetail.as_view(), name="comentarios_id-mensaje")
 
 ]
