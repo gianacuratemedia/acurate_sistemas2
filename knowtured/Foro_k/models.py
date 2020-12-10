a9f5f4e233f5934938a7de05f24ff54c9a7375d9
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-12-10 00:39:08 -0600

    Update models.py

--- knowtured/Foro_k/models.py
+++ knowtured/Foro_k/models.py
@@ -1,17 +1,32 @@
-from django.db import models
+rom django.db import models
 from Usuarios_k.models import User
 from Curso_k.models import Curso
 # Create your models here.
 class Foro(models.Model):
 
    curso_id=models.ForeignKey(to=Curso, on_delete=models.CASCADE) 
-   owner = models.ForeignKey(to=User, on_delete=models.CASCADE) 
-   mensaje= models.CharField(null=True, max_length=150)
+   owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
+   nombre= models.CharField(null=True,max_length=50) 
    created_at = models.DateTimeField(auto_now_add=True)
    updated_at = models.DateTimeField(auto_now=True)
 
-class Meta:
+   class Meta:
         ordering: ['-created_at']
 
-def __str__(self):
-        return str(self.owner)+'s message'	
+   def __str__(self):
+        return str(self.owner)+'s foro'	
+
+class Comentario(models.Model):
+
+   foro_id=models.ForeignKey(to=Foro, on_delete=models.CASCADE) 
+   owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
+   username=models.CharField(max_length=200, null=False)
+   mensaje=models.CharField(max_length=800, null=False) 
+   created_at = models.DateTimeField(auto_now_add=True)
+   updated_at = models.DateTimeField(auto_now=True)
+
+   class Meta:
+        ordering: ['-updated_at']
+
+   def __str__(self):
+        return str(self.owner)+'s comment'
