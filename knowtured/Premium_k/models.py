f7cef280492140e50b167fbb799de58bd6bea00a
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-12-22 15:36:14 -0600

    Update models.py

--- knowtured/Premium_k/models.py
+++ knowtured/Premium_k/models.py
@@ -1,30 +1,14 @@
+from django.db.models.signals import post_save
 from django.db import models
-from Usuarios_k.models import *
+from django.conf import settings
+from django.shortcuts import reverse
+from Usuarios_k.models import User
 
+class StripeCustomer(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    stripeCustomerId = models.CharField(max_length=255)
+    stripeSubscriptionId = models.CharField(max_length=255)
+    active=models.CharField(max_length=255)
 
-# Create your models here.
-"""Modelo tabla Premium"""
-class Premium(models.Model):
-     
-     usuario_id = models.ForeignKey(CustomUser,on_delete=models.CASCADE) 
-     total= models.DecimalField(null=True)
-     dias=models.IntegerField(null=True)
-     fecha_hora=models.DateTimeField('Hora de registro',auto_now_add=True)
-     no_tarjeta=models.IntegerField(null=False)
-     fecha_expiracion=models.DateField(blank=True)
-     codigo_seg=models.IntegerField(max_length=3)
-     nombre=models.CharField(null= True, max_length=150)
-     apellido_paterno=models.CharField(null= True, max_length=150)
-     apellido_materno=models.CharField(null= True, max_length=150)
-     direccion=models.CharField(null=True, max_length=100)
-     ciudad= models.CharField(null=True, max_length=50)
-     estado= models.CharField(null=True, max_length=80)
-     pais=models.CharField(null=True, max_length=80)
-     codigo_postal=models.CharField(null=True, max_length=80)
-     rfc=models.CharField(null=True, max_length=13)
-     tipo=models.CharField(null=True, max_length=20)
-     email=models.EmailField(null= False)
-     estado_compra= models.CharField(max_length=30, null=True)
-	
-def __str__(self):
-		return self.usuario_id
+    def __str__(self):
+        return self.user.username
