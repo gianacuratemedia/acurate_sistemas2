558b286bd6b965d2ddbf33a5921ec12dc3e511fa
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2020-12-22 15:35:30 -0600

    Update urls.py

--- knowtured/Premium_k/urls.py
+++ knowtured/Premium_k/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from django.conf.urls import url
 from Premium_k import views
+from .views import save_stripe_info_pa
 
 urlpatterns = [
  path('test-payment/', views.test_payment),
- path('save-stripe-info/', views.save_stripe_info),
+ path('payment-method/', views.payment_method),
+ path('save-stripe-info/plan-anual/', save_stripe_info_pa.as_view(), name="plan_anual"), 
+ path('save-stripe-info/plan-mensual/', views.save_stripe_info_pm),
  path('confirm-payment-intent/', views.confirm_payment_intent),
+ path('cancel_subscription_p/', views.CancelSub_FP), 
+ path('cancel_subscription_t/', views.CancelSub_C),
 
 ]
