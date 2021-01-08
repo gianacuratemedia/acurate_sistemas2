c3e9dfe0da746ceb17f715e168b03d57d846b7c1
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2021-01-08 15:17:02 -0600

    Update models.py

--- knowtured/Premium_k/models.py
+++ knowtured/Premium_k/models.py
@@ -1,5 +1,3 @@
-from django.db.models.signals import post_save
-from django.db import models
 from django.conf import settings
 from django.shortcuts import reverse
 from Usuarios_k.models import User
@@ -9,6 +7,14 @@ class StripeCustomer(models.Model):
     stripeCustomerId = models.CharField(max_length=255)
     stripeSubscriptionId = models.CharField(max_length=255)
     active=models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.user.username
+
+
+class StripeCustomerTransfers(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    acccount_Stripe = models.CharField(max_length=255)
 
     def __str__(self):
         return self.user.username
