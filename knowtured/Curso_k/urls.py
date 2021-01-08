dea28d76d5344318ec099b06cdc2e847f8254f50
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2021-01-08 15:23:15 -0600

    Update urls.py

--- knowtured/Curso_k/urls.py
+++ knowtured/Curso_k/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from Curso_k import views
 from .views import CursoDetail, CursoList, CursoListP, BusquedaCurso
-from .views import ContenidoDetail
-from .views import ContenidoList, ContenidoListP, Curso_Categoria_List, CursoList_Categoria_GP
+from .views import ContenidoDetail, Contenido_Usuario_Detail, Curso_Usuario_Detail
+from .views import ContenidoList, ContenidoListP, Curso_Categoria_List, CursoList_Categoria_GP, CursoPayment, ContenidoPayment
 
 urlpatterns = [
     #Lista de todos los cursos por el usuario que hace la petición get 
@@ -23,6 +24,28 @@ urlpatterns = [
     #Get contenido filtrado por curso sin permiso
     path('', ContenidoListP.as_view(), name="all_contenido"),
     #Busqueda curso por nombre
-    path('all-cursos/buscar/<str:busqueda>',BusquedaCurso.as_view(), name="curso_busqueda")
-    
+    path('all-cursos/buscar/<str:busqueda>',BusquedaCurso.as_view(), name="curso_busqueda"),
+
+    #Todos los cursos por nombre, user y no de visitas 
+    path('all-cursos/estadisticas/vistas/',CursoPayment.as_view(), name="curso_estadisticas"),
+    #Contabilizar cantidad de contenido trimestral 
+    path('all-cursos/estadisticas/contenido/',ContenidoPayment.as_view(), name="contenido_estadisticas"),
+
+    #ID's usuarios contenido
+    path('all-cursos/estadisticas/contenido/ids/', views.Contenido_ids),
+    #suma total contenido
+    path('all-cursos/estadisticas/contenido/total/', views.Contenido_total_i),
+
+    #Cantidad de contenido total creado por usuario 
+    path('all-cursos/estadisticas/contenido/total/<int:owner>',Contenido_Usuario_Detail.as_view(), name="no_contenido_user"),
+
+    #Obteniendo datos de cursos y vistas trimestrales 
+    path('all-cursos/estadisticas/cursos/ids/', views.Vistas_Cursos_ids),   
+
+    #Suma de todas las vistas trimestrales totales de los cursos 
+    path('all-cursos/estadisticas/cursos/total/', views.Curso_total_i),
+
+    #Cantidad de vistas trimestrales totales creadas por usuario
+    path('all-cursos/estadisticas/contenido/total/<int:owner>',Curso_Usuario_Detail.as_view(), name="no_vistas_curso_user"),
+
 ]
