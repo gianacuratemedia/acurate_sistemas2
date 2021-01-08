7f3ac4280601ea68c2008a1e819cf5897753287b
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2021-01-08 15:26:20 -0600

    Update admin.py

--- knowtured/Curso_k/admin.py
+++ knowtured/Curso_k/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Curso, Contenido
+from .models import Curso, Contenido, Contenido_Usuario
 
 admin.site.register(Curso)
 admin.site.register(Contenido)
+admin.site.register(Contenido_Usuario)
