6e391b70290162d3a59c775c3bf805798a9579c2
JenniGarcia234 <55256599+JenniGarcia234@users.noreply.github.com>
2021-01-08 15:18:39 -0600

    Update admin.py

--- knowtured/Premium_k/admin.py
+++ knowtured/Premium_k/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import StripeCustomer
+from .models import StripeCustomer, StripeCustomerTransfers
 
 # Register your models here.
 
 admin.site.register(StripeCustomer)
+admin.site.register(StripeCustomerTransfers)
